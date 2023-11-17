8b218e8293c43e90d32d3f2813800dc075dfd744
PrasantaDebnath25 <prasantadebnath.2013@gmail.com>
2023-11-17 11:48:46 +0530

    Search with multiple addded

---
+++ src/component/MultipleSelection/SearchCasesInput.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import AsyncSelect from "react-select";
+
+const SearchCasesInput = (props) => {
+  const {
+    caseListForSelect,
+    selectCases,
+    selectedCases,
+    loadMoreCases,
+    searchCases,
+  } = props;
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleChange = (data) => {
+    const seeMore = data[data.length - 1];
+    if (data.length > 0 && !seeMore?.id) {
+      loadMoreCases();
+      setIsMenuOpen(true);
+    } else {
+      selectCases(data);
+    }
+  };
+
+  return (
+    <AsyncSelect
+      id="cases"
+      isMulti
+      isClearable={true}
+      isLoading={false}
+      isSearchable={true}
+      options={caseListForSelect}
+      onChange={handleChange}
+      onInputChange={(value) => searchCases(value)}
+      placeholder="Select Multiple Case..."
+      value={selectedCases}
+      menuIsOpen={isMenuOpen}
+      onFocus={() => setIsMenuOpen(true)}
+      onBlur={() => setIsMenuOpen(false)}
+    />
+  );
+};
+
+export default SearchCasesInput;
